7379406680ed59ce7ec54b2175bd87a226761192
legendalive <54436272+legendalive@users.noreply.github.com>
2026-09-26 01:49:23 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,20 @@
 import type { NextConfig } from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+}
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: '/cryptorium',
-  assetPrefix: '/cryptorium/',
+  basePath: repo ? `/${repo}` : '',
+  assetPrefix: repo ? `/${repo}/` : '',
 };
 
 export default nextConfig;
